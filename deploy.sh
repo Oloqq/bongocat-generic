7fb843401c893d39df486d9124ed1317f2435203
Oloqq <olgierdpk@gmail.com>
2022-08-22 19:37:59 +0200

    deploy with custom name

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
+
+name=$1
+if [ -z "$name" ]
+then
+    name="bongo"
+fi
+
 rm -rf bin/deploy
-rm -f  bin/bongo.exe
+rm -f  bin/*.exe
+rm -f  bin/*.zip
 
 cmd.exe /c "make"
 
 mkdir            bin/deploy
 cp bin/bongo.exe bin/deploy
 cp config.json   bin/deploy
+cp changelog     bin/deploy
 cp WhyFork.md    bin/deploy
 cp -r img        bin/deploy
 cp -r font       bin/deploy
 
-zip -r bin/bongo.zip bin/deploy
+zip -rq bin/$name bin/deploy
